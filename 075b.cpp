8c274834254ea9b825646e6f43584be555914fb7
haruki-mtn <haruki.mtn@gmail.com>
2025-06-10 12:13:15 +0900

    2025-06-10_12:13:15

--- 075b.cpp
+++ 075b.cpp
@@ -4,60 +4,42 @@ using namespace std;
 int main() {
     int h, w;
     cin >> h >> w;
-    vector<vector<char>> before(h, vector<char>(w));
+    vector<vector<char>> board(h, vector<char>(w));
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
-            cin >> before.at(i).at(j);
+            cin >> board.at(i).at(j);
         }
     }
 
-    vector<vector<char>> after(h, vector<char>(w));
+    const vector<int> DX = {-1, 0, 1};
+    const vector<int> DY = {-1, 0, 1};
+
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
+            if (board.at(i).at(j) == '#') continue;
+
             int cnt = 0;
-            if (before.at(i).at(j) == '.') {
-                if (before.at(i - 1).at(j) == '#') {
-                    cnt++;
+            for (int dx : DX) {
+                for (int dy : DY) {
+                    if (dx == 0 && dy == 0) continue;
+                    int x = j + dx;
+                    int y = i + dy;
+                    if (x >= 0 && x < w && y >= 0 && y < h) {
+                        if (board.at(y).at(x) == '#') {
+                            cnt++;
+                        }
+                    }
                 }
-                if (before.at(i - 1).at(j + 1)) {
-                    cnt++;
-                }
-                if (before.at(i).at(j + 1)) {
-                    cnt++;
-                }
-                if (before.at(i + 1).at(j + 1)) {
-                    cnt++;
-                }
-                if (before.at(i + 1).at(j)) {
-                    cnt++;
-                }
-                if (before.at(i + 1).at(j - 1)) {
-                    cnt++;
-                }
-                if (before.at(i).at(j - 1)) {
-                    cnt++;
-                }
-                if (before.at(i - 1).at(j - 1)) {
-                    cnt++;
-                }
-                if (cnt != 0) {
-                    after.at(i).at(j) = (char)cnt;
-                }
-                after.at(i).at(j) = (char)cnt;
-            } else {
-                after.at(i).at(j) = '#';
             }
+            board.at(i).at(j) = char('0' + cnt);
         }
     }
 
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
-            if (j == w - 1) {
-                cout << after.at(i).at(j) << '\n';
-            } else {
-                cout << after.at(i).at(j);
-            }
+            cout << board.at(i).at(j);
         }
+        cout << '\n';
     }
     return 0;
 }
